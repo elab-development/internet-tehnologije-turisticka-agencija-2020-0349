b769d5681cb73b1c44fa9dfcbdf06ee0cabb5bd4
Mihailo222 <92820769+Mihailo222@users.noreply.github.com>
2024-08-30 17:56:31 +0200

    dodavanje nove logike

--- server.py
+++ server.py
@@ -7,7 +7,26 @@ app = Flask(__name__)
 
 
 
+@app.route('/chat', methods=['POST'])
+def chat_with_llm():
+    data = request.json
+    print(data)
+    prompt_message = data.get('prompt')
 
+    try:
+        GROQ_API_KEY = "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"
+        chat = ChatGroq(temperature=0, model_name="llama3-8b-8192", groq_api_key=GROQ_API_KEY)
+        system = """You are a helpful assistant. You know only what is told you to do. You don't have any other knowledge
+        about world at all. 
+        """
+        human = "{text}"
+        prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human),("placeholder","{agent_scratchpad}")])
+        chain = prompt | chat 
+        response = chain.invoke({"text":prompt_message})
+        print(response) #.content
+        return jsonify({'response': response.content})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 
