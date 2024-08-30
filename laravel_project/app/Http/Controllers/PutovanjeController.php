b5f08e5965785dbe4b17ff0af965b5cb005f034e
zoranmahovac <zmahovac@gmail.com>
2024-08-30 17:20:59 +0200

    dodavanje controllera za Aranzman

---
+++ laravel_project/app/Http/Controllers/PutovanjeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Putovanje;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use App\Http\Resources\PutovanjeResource;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+
+
+
+class PutovanjeController extends Controller
+{
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ laravel_project/app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    
+}
