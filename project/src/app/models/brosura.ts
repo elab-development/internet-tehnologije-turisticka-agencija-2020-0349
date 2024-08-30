a80eea1da7f37915363f97de9084921e2079b4f4
Mihailo222 <92820769+Mihailo222@users.noreply.github.com>
2024-08-30 15:54:13 +0200

    dodat je interfejs brosura

--- project/src/app/models/brosura.ts
+++ project/src/app/models/brosura.ts
@@ -1,2 +1,10 @@
 export interface Brosura {
+      season: string, 
+    start_date: Date,
+    end_date: Date,
+    avg_price: number,
+    id: number,
+    picture: string,
+    description: string,
+
 }
