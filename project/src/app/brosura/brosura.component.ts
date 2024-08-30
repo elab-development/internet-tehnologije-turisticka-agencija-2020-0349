91277913e38bc06884009e7f19b31f511429fb12
Mihailo222 <92820769+Mihailo222@users.noreply.github.com>
2024-08-30 15:42:19 +0200

    dodate akcije za brosuru, za povlacenje podataka iz baze

--- project/src/app/brosura/brosura.component.ts
+++ project/src/app/brosura/brosura.component.ts
@@ -8,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrl: './brosura.component.scss'
 })
 export class BrosuraComponent {
+  brosure: Brosura[] = [];
+
+
+  constructor(private brosuraService: BrosuraService){}
+  
+  //u vreme incijializacije stranice
+  ngOnInit(){
+  
+  this.fetchBrosure();
+  console.log(this.brosure);
+  
+  
+  }
+  
+  
+  
+  
+  fetchBrosure(): void {
+  
+    this.brosuraService.getBrosure().subscribe(
+    (data) => {
+  
+      this.brosure = data;
+      console.log(this.brosure);
+    },
+    (error) => {
+      console.log("Error fetching brosure:",error);
+    }
+  
+  
+  
+    );
+  
+  
+  
+  
+  }
 
 }
