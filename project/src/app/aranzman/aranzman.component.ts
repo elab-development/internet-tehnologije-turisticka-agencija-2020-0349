41d3a98d6cf18f4992e1de379bf3c1f0683012ab
zoranmahovac <zmahovac@gmail.com>
2024-08-30 15:28:25 +0200

    dodata je komponenta brosura - html,scss,ts

---
+++ project/src/app/aranzman/aranzman.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aranzman',
+  standalone: true,
+  imports: [],
+  templateUrl: './aranzman.component.html',
+  styleUrl: './aranzman.component.scss'
+})
+export class AranzmanComponent {
+
+}
---
+++ project/src/app/brosura/brosura.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brosura',
+  standalone: true,
+  imports: [],
+  templateUrl: './brosura.component.html',
+  styleUrl: './brosura.component.scss'
+})
+export class BrosuraComponent {
+
+}
