46aa346ac04c29516bbc2df56839f934750bd177
zoranmahovac <zmahovac@gmail.com>
2024-08-30 16:14:30 +0200

    dodate rute, profil komponenta, admin,user-editing

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,8 +1,25 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './auth.guard';
+import { UserEditingComponent } from './user-editing/user-editing.component';
+import { AranzmanComponent } from './aranzman/aranzman.component';
+import { ChatComponent } from './chat/chat.component';
 
 export const routes: Routes = [
 
+    //{ path: 'home', component: HomeComponent },
     { path: 'home', component: HomeComponent },
-
+    { path: 'login', component: LoginComponent },
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard], },
+    { path: 'register', component: RegisterComponent },
+    { path: 'users/:id/edit', component: UserEditingComponent}, 
+    { path:'aranzman', component:AranzmanComponent},
+    {path:'chat',component:ChatComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to home
+  
 ];
