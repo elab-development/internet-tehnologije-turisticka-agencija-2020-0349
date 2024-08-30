f940ed0d239e5d40b2603fcb7b7e9a7aa0ba2544
Mihailo222 <92820769+Mihailo222@users.noreply.github.com>
2024-08-30 16:17:11 +0200

    dodata celokupna logika za admina

--- project/src/app/admin-profile/admin-profile.component.ts
+++ project/src/app/admin-profile/admin-profile.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import {  Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user';
+import { AranzmanService } from '../services/aranzman.service';
+import { Aranzman } from '../models/aranzman';
+
 
 @Component({
   selector: 'app-admin-profile',
@@ -8,5 +15,76 @@ import { Component } from '@angular/core';
   styleUrl: './admin-profile.component.scss'
 })
 export class AdminProfileComponent {
+
+constructor(private authService: AuthService, private router:Router, private userService: UserService, private aranzmanService: AranzmanService){}
+userData:any;
+users:User[];
+
+aranzmani: Aranzman[]=[];
+
+
+ngOnInit(): void{
+
+  this.getUserInfo();
+  this.getUsers();
+  this.getAllAranzmani();
+  
+
+ 
+}
+
+
+getUserInfo(){
+  this.userService.getProfile().subscribe(
+    (res) => {
+      this.userData=res;
+      console.log("RESSS",res);
+    }
+  )
+}
+
+logout(){
+  this.authService.logout().subscribe(
+  (res) => {
+    localStorage.removeItem('token');
+    this.router.navigate(['login']);
+    console.log(res);
+   
+  }
+  );
+}
+
+
+getUsers(){
+  this.userService.getUsers().subscribe(
+    (res)=>{
+      this.users = res;
+      console.log(this.users);
+    }
+  )
+}
+
+deleteUser(user:User){
+  this.userService.deleteUser(user).subscribe(
+    (res) => {
+      console.log(res);
+      this.getUsers();
+    }
+  );
+
+}
+
+
+getAllAranzmani(){
+  this.aranzmanService.getAranzmani().subscribe(
+
+    (res) => {
+      
+      this.aranzmani=res;
+      console.log(res);}
+  )
+}
+
+
 
 }
