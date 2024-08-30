65d599cff3ab18ff87ae1ad901d29fa0ee1e7fc2
Mihailo222 <92820769+Mihailo222@users.noreply.github.com>
2024-08-30 17:06:12 +0200

    implementacija klase Agencija modela

--- laravel_project/app/Models/Agencija.php
+++ laravel_project/app/Models/Agencija.php
@@ -8,5 +8,18 @@ use App\Models\Agencija;
 
 class Agencija extends Model
 {
+    use HasFactory;
+
+    protected $table='agencija';
+
+
+    protected $fillable = [
+       'id', 'naziv',	'adresa',	'telefon',	'gmail', 
+    ];
+    
+
+    public function aranzmani() {
+        return $this->hasMany(Aranzman::class);
+    }
     
 }
