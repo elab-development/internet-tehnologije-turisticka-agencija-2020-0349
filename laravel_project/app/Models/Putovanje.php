f0a7dd7c90141ece7c5f1b3565708b25521e194b
zm20200109 <147525494+zm20200109@users.noreply.github.com>
2024-08-30 17:10:26 +0200

    implementacija putovanja

--- laravel_project/app/Models/Putovanje.php
+++ laravel_project/app/Models/Putovanje.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Putovanje extends Model
 {
     
+use HasFactory;
+
+
+
+    
+    public $table='putovanje';
+
+    protected $fillable = [
+       'id', 'user_id', 'aranzman_id',
+    ];
+
+
+
+
+    public function aranzman() {
+        return $this->belongsTo(Aranzman::class);
+    }
+    
+    
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    
 }
