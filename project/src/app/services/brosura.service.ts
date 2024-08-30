25b3d0752a7b1961dca42264d2f278d2b8731817
Mihailo222 <92820769+Mihailo222@users.noreply.github.com>
2024-08-30 16:02:11 +0200

    implementirana logika brosura servisa

--- project/src/app/services/brosura.service.ts
+++ project/src/app/services/brosura.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrosuraService {
 
-  constructor() { }
+url: string = 'http://localhost:8000/api';
+  constructor(private http:HttpClient){}
+
+  getBrosure(): Observable<any>{
+
+    return this.http.get(`${this.url}/brosure`);
+  }
 }
